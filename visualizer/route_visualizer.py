9f5d100d77fab236f000c07f37422d8004ee00e2
kibuna <task.aizawa@gmail.com>
2023-01-03 21:33:12 +0900

    add comparison mode to visualizer

--- visualizer/route_visualizer.py
+++ visualizer/route_visualizer.py
@@ -87,47 +87,136 @@ if uploaded_file is not None:
         scores.append(int(score) / 1000000)
         paths.append([int(p) for p in path[:int(length)]])
 
+    is_comparison = st.sidebar.checkbox("Compare two paths")
+    if not is_comparison:
 
-    options = pd.DataFrame({"score": [f"{i}: {s}" for i, s in enumerate(scores)]})
+        options = pd.DataFrame({"score": [f"{i}: {s}" for i, s in enumerate(scores)]})
 
-    chosen = st.sidebar.selectbox("Select path to show", options, len(options)-1)
-    idx, score = chosen.split(":")
-    idx = int(idx)
-    path = paths[idx]
+        chosen = st.sidebar.selectbox("Select path to show", options, len(options)-1)
+        idx, score = chosen.split(":")
+        idx = int(idx)
+        path = paths[idx]
 
-    pos = []
-    for r in df_image.itertuples():
-        pos.append((r.x, r.y))
-    path = [pos[r-1] for r in path]
-    for i in range(len(path)):
-        if path[i] == (0, 0):
-            start = i
-    path = path[start:] + path[:start]
-    path = [cartesian_to_array(*p, image.shape) for p in path]
-    path = [(p[0]-128, p[1]-128, i) for i, p in enumerate(path)]    
+        pos = []
+        for r in df_image.itertuples():
+            pos.append((r.x, r.y))
+        path = [pos[r-1] for r in path]
+        for i in range(len(path)):
+            if path[i] == (0, 0):
+                start = i
+        path = path[start:] + path[:start]
+        path = [cartesian_to_array(*p, image.shape) for p in path]
+        path = [(p[0]-128, p[1]-128, i) for i, p in enumerate(path)]    
 
-    st.text("best theoretical score with optimal arm configs:")
-    st.text(calc_costs([p[:2] for p in path] + [(0, 0)]))
-    
-    path_range = st.sidebar.slider("Index range to plot", 0, len(path), (0, len(path)))
+        st.text("best theoretical score with optimal arm configs:")
+        st.text(calc_costs([p[:2] for p in path] + [(0, 0)]))
+        
+        path_range = st.sidebar.slider("Index range to plot", 0, len(path), (0, len(path)))
 
-    pathnp = np.array(path)
-    pathnp = pathnp[path_range[0]:path_range[1]]
+        pathnp = np.array(path)
+        pathnp = pathnp[path_range[0]:path_range[1]]
 
-    if not is_color:
-        s = ColumnDataSource({"xpos": pathnp[:, 0], "ypos": pathnp[:, 1], "index": pathnp[:, 2]})
-        p.line("xpos", "ypos", source=s)
-        p.scatter("xpos", "ypos", source=s, size=1.5)
-    else:
-        s = ColumnDataSource({"xposs": [pathnp[i:i+2, 0] for i in range(pathnp.shape[0]-1)], "yposs": [pathnp[i:i+2, 1] for i in range(pathnp.shape[0]-1)], "color": pathnp[:, 2] / pathnp.shape[0], "index": pathnp[:, 2],
-                                "xpos": pathnp[:, 0], "ypos": pathnp[:,1]})
-        color_mapper = LinearColorMapper(palette=colorcet.rainbow4, low=np.min(pathnp[:, 2]), high=np.max(pathnp[:, 2]))
-        glyph = MultiLine(xs="xposs", ys="yposs", line_color={"field": "index", "transform": color_mapper}, line_width=1)
-        cb = ColorBar(color_mapper=color_mapper)
-        p.add_glyph(s, glyph)
-        p.add_layout(cb, "right")
-    
-    st.sidebar.download_button("Download this solution", f"""{lengths[idx]} {int(scores[idx]*1000000)}\n{' '.join([str(p) for p in paths[idx]])}""", file_name=f"{idx}_{float(score)}.txt")
+        if not is_color:
+            s = ColumnDataSource({"xpos": pathnp[:, 0], "ypos": pathnp[:, 1], "index": pathnp[:, 2]})
+            p.line("xpos", "ypos", source=s)
+            p.scatter("xpos", "ypos", source=s, size=1.5)
+        else:
+            s = ColumnDataSource({"xposs": [pathnp[i:i+2, 0] for i in range(pathnp.shape[0]-1)], "yposs": [pathnp[i:i+2, 1] for i in range(pathnp.shape[0]-1)], "color": pathnp[:, 2] / pathnp.shape[0], "index": pathnp[:, 2],
+                                    "xpos": pathnp[:, 0], "ypos": pathnp[:,1]})
+            color_mapper = LinearColorMapper(palette=colorcet.rainbow4, low=np.min(pathnp[:, 2]), high=np.max(pathnp[:, 2]))
+            glyph = MultiLine(xs="xposs", ys="yposs", line_color={"field": "index", "transform": color_mapper}, line_width=1)
+            cb = ColorBar(color_mapper=color_mapper)
+            p.add_glyph(s, glyph)
+            p.add_layout(cb, "right")
+        
+        st.sidebar.download_button("Download this solution", f"""{lengths[idx]} {int(scores[idx]*1000000)}\n{' '.join([str(p) for p in paths[idx]])}""", file_name=f"{idx}_{float(score)}.txt")
+    else: # comparison mode
+        ignore_direction = st.sidebar.checkbox("ignore direction in path comparison")
+        options = pd.DataFrame({"score": [f"{i}: {s}" for i, s in enumerate(scores)]})
 
+        chosen1 = st.sidebar.selectbox("Select path to compare 1", options, 0)
+        chosen2 = st.sidebar.selectbox("Select path to compare 2", options, 1)
+        idx1, score1 = chosen1.split(":")
+        idx1 = int(idx1)
+        path1 = paths[idx1]
+        idx2, score2 = chosen2.split(":")
+        idx2 = int(idx2)
+        path2 = paths[idx2]
 
+        pos = []
+        for r in df_image.itertuples():
+            pos.append((r.x, r.y))
+        path1 = [pos[r-1] for r in path1]
+        path2 = [pos[r-1] for r in path2]
+
+        for i in range(len(path1)):
+            if path1[i] == (0, 0):
+                start1 = i
+        path1 = path1[start1:] + path1[:start1]
+        path1 = [cartesian_to_array(*p, image.shape) for p in path1]
+        path1 = [(p[0]-128, p[1]-128, i) for i, p in enumerate(path1)]    
+        path1 = np.array(path1)
+
+        for i in range(len(path2)):
+            if path2[i] == (0, 0):
+                start2 = i
+        path2 = path2[start2:] + path2[:start2]
+        path2 = [cartesian_to_array(*p, image.shape) for p in path2]
+        path2 = [(p[0]-128, p[1]-128, i) for i, p in enumerate(path2)]    
+        path2 = np.array(path2)
+
+        st.text("best theoretical score with optimal arm configs:")
+        st.text(calc_costs([p[:2] for p in path1] + [(0, 0)]))
+        st.text(calc_costs([p[:2] for p in path2] + [(0, 0)]))
+
+        path1edge = []
+        path2edge = []
+        set_path1edge = set()
+        set_path2edge = set()
+
+        def to_tuple(a):
+            try:
+                return tuple(to_tuple(i) for i in a)
+            except:
+                return a
+
+        for i in range(len(path1) -1):
+            path1edge.append(path1[i:i+2])
+            path2edge.append(path2[i:i+2])
+            set_path1edge.add(to_tuple(path1[i:i+2, 0:2]))
+            set_path2edge.add(to_tuple(path2[i:i+2, 0:2]))
+            if ignore_direction:
+                set_path1edge.add(to_tuple(path1[i+1:i-1:-1, 0:2]))
+                set_path2edge.add(to_tuple(path2[i+1:i-1:-1, 0:2]))
+
+        path1only = []
+        path2only = []
+
+        for i in range(len(path1) - 1):
+            if to_tuple(path1[i:i+2, 0:2]) not in set_path2edge:
+                path1only.append(path1[i:i+2])
+            if to_tuple(path2[i:i+2, 0:2]) not in set_path1edge:
+                path2only.append(path2[i:i+2])
+
+        vals = st.sidebar.multiselect("Path to plot", ["1", "2", "only1", "only2"], default=["only1", "only2"])
+
+        if "1" in vals:
+            s1 = ColumnDataSource({"xpos": [p[:, 0] for p in path1edge], "ypos": [p[:, 1] for p in path1edge], "index": [p[:, 2] for p in path1edge]})
+            glyph1 = MultiLine(xs="xpos", ys="ypos", line_color="blue", line_width=1, line_alpha=0.5)
+            p.add_glyph(s1, glyph1)
+        if "2" in vals:
+            s1 = ColumnDataSource({"xpos": [p[:, 0] for p in path2edge], "ypos": [p[:, 1] for p in path2edge], "index": [p[:, 2] for p in path2edge]})
+            glyph1 = MultiLine(xs="xpos", ys="ypos", line_color="blue", line_width=1, line_alpha=0.5)
+            p.add_glyph(s1, glyph1)
+        if "only1" in vals:
+            s1 = ColumnDataSource({"xpos": [p[:, 0] for p in path1only], "ypos": [p[:, 1] for p in path1only], "index": [p[:, 2] for p in path1only]})
+            glyph1 = MultiLine(xs="xpos", ys="ypos", line_color="blue", line_width=2, line_alpha=0.8)
+            p.add_glyph(s1, glyph1)
+        if "only2" in vals:
+            s1 = ColumnDataSource({"xpos": [p[:, 0] for p in path2only], "ypos": [p[:, 1] for p in path2only], "index": [p[:, 2] for p in path2only]})
+            glyph1 = MultiLine(xs="xpos", ys="ypos", line_color="blue", line_width=2, line_alpha=0.8)
+            p.add_glyph(s1, glyph1)
+
+        # st.text(path1only)
+        # st.text(path2only)
 st.bokeh_chart(p, use_container_width=False)
