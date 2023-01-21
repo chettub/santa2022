d92e346116f737675a1b56428c9f33664a6c3663
kibuna <task.aizawa@gmail.com>
2023-01-21 15:56:30 +0900

    clean up

--- gpx2/debug-compile.sh
+++
@@ -1,1 +0,0 @@
-g++ file_man.cpp global.cpp gpx.cpp int128.cpp main.cpp util_functions.cpp -std=c++17 -o gpx2 -O0 -g
--- gpx2/global.cpp
+++
@@ -1,7 +0,0 @@
-#include "defs.h"
-
-// TSP
-double *coord_x, *coord_y;  // coordinates of the cities
-int** W;                    // weight matrix for the ATSP
-int n_cities = 257 * 257;   // number of cities
-char* prob_name;            // name of the file for the weight matrix
